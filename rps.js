47feccd1bf0a85b6253b53d1540e7a55aab2037d
CaptainAyal <ayalmesher@gmail.com>
2025-01-24 16:59:42 +0200

    computer choice function complete

--- rps.js
+++ rps.js
@@ -1,1 +1,18 @@
-console.log("hello")
+
+
+function getComputerChoice(){
+// Create function getComputerChoice
+let choice = Math.floor(Math.random() * 3)
+// sets choice variable to 0, 1, or 2
+console.log (choice)
+if (choice == 0) {
+    return "rock"
+    }
+if (choice == 1) {
+    return "paper"
+} else {
+    return "scissors"
+}
+}
+
+// getComputerChoice randomly returns strings rock paper or scissors
