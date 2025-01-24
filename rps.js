79bbb70d577c79f070c212b98b312de8d7a88cdc
CaptainAyal <ayalmesher@gmail.com>
2025-01-24 17:39:25 +0200

    human prompt selection done. Errors on capital, need fix

--- rps.js
+++ rps.js
@@ -8,7 +8,7 @@ console.log (choice)
 if (choice == 0) {
     return "rock"
     }
-if (choice == 1) {
+else if (choice == 1) {
     return "paper"
 } else {
     return "scissors"
@@ -16,3 +16,26 @@ if (choice == 1) {
 }
 
 // getComputerChoice randomly returns strings rock paper or scissors
+
+function getHumanChoice(){
+    let hNum
+    // declare variable
+    let hChoice = prompt("Please choose 'rock', 'paper', or 'scissors'.")
+    // get user choice
+    // hChoice.toLowerCase()=newChoice **not working
+    //solve for upper/lower case issues
+    console.log(hChoice)
+    if (hChoice == "rock") {
+        hNum = 1
+    }
+    else if (hChoice == "paper") {
+        hNum = 2
+    } 
+    else if (hChoice == "scissors"){
+        hNum = 3
+    }
+    else{
+        alert("Invalid Choice")
+    }    
+    console.log(hNum)
+    }
